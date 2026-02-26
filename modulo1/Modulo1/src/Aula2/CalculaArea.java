5b48bc1486110fa0a797caea29fe06e651f3678d
André Herieberto Schmitt <andreherieberto@furb.br>
2026-02-25 21:42:40 -0300

    area quadrado

---
+++ modulo1/Modulo1/src/Aula2/CalculaArea.java
@@ -0,0 +1,14 @@
+package Aula2;
+
+public class CalculaArea {
+    public static void main(String[] args) {
+
+        float altura = 3;
+        float largura = 5;
+
+        float area = altura * largura;
+
+        System.out.println(area);
+
+    }
+}
--- modulo1/Modulo1/src/Aula2/MediaPonderada.java
+++ modulo1/Modulo1/src/Aula2/MediaPonderada.java
@@ -1,3 +1,5 @@
+package Aula2;
+
 public class MediaPonderada {
     public static void main(String[] args) {
 
