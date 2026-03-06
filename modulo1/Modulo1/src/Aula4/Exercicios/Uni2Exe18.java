7a1804763ce9a00f9c4896f86be053ae9e83b2ba
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-03-06 14:21:46 -0300

    18 parcial

---
+++ modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe18.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Uni2Exe18{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o preço unitário do azulejo: ");
+        
+
+        scan.close();
+    }
+}
