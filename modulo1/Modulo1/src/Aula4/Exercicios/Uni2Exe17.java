4fa0e12e8b59a6a26aa42c450d1aed067eff7ffd
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-03-06 14:03:47 -0300

    17 completed

--- modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe17.java
+++ modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe17.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Uni2Exe17{
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome;
+        float horasTrabalhadas, salarioTrabalho, salarioFamilia, salarioBruto, salarioLiquido;
+        int numeroDependentes;
+        double taxas = 0.085 + 0.05;
+
+        System.out.print("Digite o nome do funcionário: ");
+        nome = scan.nextLine();
+        System.out.print("Digite o total de horas trabalhadas pelo funcionário: ");
+        horasTrabalhadas = scan.nextFloat();
+        System.out.print("Digite o número de dependentes do funcionário: ");
+        numeroDependentes = scan.nextInt();
+
+        salarioTrabalho = 10*horasTrabalhadas;
+        salarioFamilia = 60*numeroDependentes;
+        salarioBruto = salarioTrabalho+salarioFamilia;
+        salarioLiquido = salarioBruto - (float) (salarioTrabalho*taxas);
+
+        System.out.println("Dados do funcionário "+nome+": ");
+        System.out.printf("Salário Bruto: R$%.2f%n", salarioBruto);
+        System.out.printf("Salário Líquido: R$%.2f", salarioLiquido);
         
+
+        scan.close();
     }
 }
