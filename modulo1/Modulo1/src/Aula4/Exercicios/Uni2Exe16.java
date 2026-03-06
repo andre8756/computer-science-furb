147313fa4a0919f6c6ea01c939408b8a8d24bae7
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-03-06 07:48:37 -0300

    15 completed

---
+++ modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe16.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class Uni2Exe16{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        float valor = 50.25f;
+        valor = valor % 50;
+        System.out.println(valor);
+
+    }
+}
