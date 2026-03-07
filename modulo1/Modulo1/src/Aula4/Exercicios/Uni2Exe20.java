96f204e80ffaa9553cbe4ea06348e0185601f6c7
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-03-07 15:14:56 -0300

    20

---
+++ modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe20.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Uni2Exe20{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int dobras, quadrados;
+
+        System.out.print("Digite o número de dobras (em um número par) feitas no papel: ");
+        dobras = scan.nextInt();
+
+        quadrados = (int) Math.pow(2, dobras);
+
+        System.out.printf("Ao desdobrar você verá %d quadrados.", quadrados);
+
+        scan.close();
+    }
+}
