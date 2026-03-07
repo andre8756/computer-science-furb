85d6ba65e16f1f88010a72ba21b10609ed83b526
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-03-07 14:51:26 -0300

    18 and 19

--- modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe18.java
+++ modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe18.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 public class Uni2Exe18{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        float altura, comprimento, preco;
+        float precoUnitario = 12.5f;
 
-        System.out.print("Digite o preço unitário do azulejo: ");
-        
+        System.out.print("Digite o comprimento da parede em metros: ");
+        comprimento = scan.nextFloat();
+        System.out.print("Digite a altura da parede em metros: ");
+        altura = scan.nextFloat();
 
+        preco = ((comprimento * altura) * 9) * precoUnitario;
+
+        System.out.printf("O valor gasto com a compra de azulejos é um total de R$ %.2f", preco);
         scan.close();
     }
 }
---
+++ modulo1/Modulo1/src/Aula4/Exercicios/Uni2Exe19.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Uni2Exe19{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a, b, c, d;
+
+        System.out.print("Digite o valor de B: ");
+        b = scan.nextDouble();
+        System.out.print("Digite o valor de C: ");
+        c = scan.nextDouble();
+        System.out.print("Digite o valor de D: ");
+        d = scan.nextDouble();
+
+        a = b;
+
+        System.out.printf("Valor de A = %.2f", a);
+        
+
+        scan.close();
+    }
+}
