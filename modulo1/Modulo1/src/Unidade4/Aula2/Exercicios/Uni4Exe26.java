0ccffd19ba442c1ef9ceeabf176b865a92ee6fe4
André Heriberto Schmitt <andreherieberto@furb.br>
2026-04-09 21:39:57 -0300

    27

---
+++ modulo1/Modulo1/src/Unidade4/Aula2/Exercicios/Uni4Exe26.java
@@ -0,0 +1,19 @@
+package Unidade4.Aula2.Exercicios;
+
+import java.util.Scanner;
+
+public class Uni4Exe26 {
+    Scanner scan = new Scanner(System.in);
+    char opcao;
+
+    public void iniciar() {
+        System.out.println("Escolha uma opção");
+        System.out.println("T: calcular a área de um triângulo de base b e a altura h");
+        System.out.println("Q: calcular a área de um quadrado de lado l");
+        System.out.println("R: calcular a área de um retângulo de base b e altura h");
+        System.out.println("C: calcular a área de um círculo de raio r");
+        System.out.println();
+        System.out.println(">> ");
+
+    }
+}
