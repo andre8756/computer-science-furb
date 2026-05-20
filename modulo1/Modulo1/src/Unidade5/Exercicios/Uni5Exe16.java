7fda40384724ca9a6794dd8cb52c1e623c302b91
André Heriberto Schmitt <andreherieberto@furb.br>
2026-05-20 19:55:25 -0300

    17

--- modulo1/Modulo1/src/Unidade5/Exercicios/Uni5Exe16.java
+++ modulo1/Modulo1/src/Unidade5/Exercicios/Uni5Exe16.java
@@ -7,20 +7,36 @@ public class Uni5Exe16 {
         Scanner scan = new Scanner(System.in);
         float altura = 1;
         char genero;
+        float alturaGrupoMulheres = 0;
+        float alturaGeral = 0;
+        int qntMulheres = 0;
+        int qntGrupo = 0;
 
         try {
-            while (altura != 0) {
+            while (altura > 0) {
 
-                System.out.println("Atura: ");
+                System.out.print("Atura: ");
                 altura = scan.nextFloat();
-                System.out.println("Gênero: ");
-                genero = scan.next().toUpperCase().charAt(0);
 
-                if (genero == 'M') {
-
+                if (altura <= 0) {
+                    break;
                 }
 
+                System.out.print("Gênero: ");
+                genero = scan.next().toUpperCase().charAt(0);
+
+                if (genero == 'F') {
+                    alturaGrupoMulheres += altura;
+                    qntMulheres++;
+                }
+
+                alturaGeral += altura;
+                qntGrupo += altura;
             }
+
+            System.out.printf("A média da altura das mulheres é: %.2f \n", (alturaGrupoMulheres / qntMulheres));
+            System.out.printf("A média da altura do grupo é: %.2f \n", (alturaGeral / qntGrupo));
+
         } catch (Exception ex) {
             System.out.println("Algo deu errado :/");
         }
---
+++ modulo1/Modulo1/src/Unidade5/Exercicios/Uni5Exe17.java
@@ -0,0 +1,5 @@
+package Unidade5.Exercicios;
+
+public class Uni5Exe17 {
+
+}
