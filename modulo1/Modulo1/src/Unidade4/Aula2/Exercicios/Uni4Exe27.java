4bdb1d876486131f7bdfb997595acd6d89b7c981
André Heriberto Schmitt <andreherieberto@furb.br>
2026-06-11 21:25:36 -0300

    teste

--- modulo1/Modulo1/src/Unidade4/Aula2/Exercicios/Uni4Exe27.java
+++ modulo1/Modulo1/src/Unidade4/Aula2/Exercicios/Uni4Exe27.java
@@ -1,15 +1,17 @@
+package Unidade4.Aula2.Exercicios;
+
 import java.util.Scanner;
 
-public class Uni4Exe27{
+public class Uni4Exe27 {
     Scanner scan = new Scanner(System.in);
     int horaChegada, minutoChegada, horaSaida, minutoSaida, horaPagar, minutoPagar;
     float preco;
 
-    public Uni4Exe27(){
+    public Uni4Exe27() {
         iniciar();
     }
 
-    public void iniciar(){
+    public void iniciar() {
         System.out.println("Digite apenas a hora de chegada (hh): ");
         horaChegada = scan.nextInt();
         System.out.println("Digite o minuto de chegada(mm): ");
@@ -23,20 +25,20 @@ public class Uni4Exe27{
         calcularEstacionamento();
     }
 
-    public int calcularEstacionamento(){
+    public int calcularEstacionamento() {
 
         horaPagar = horaSaida - horaChegada;
         minutoPagar = minutoSaida - minutoChegada;
 
         if (minutoPagar < 0) {
-        minutoPagar += 60;
-        horaPagar--;
+            minutoPagar += 60;
+            horaPagar--;
         }
-        
-        if(horaChegada > horaSaida){
+
+        if (horaChegada > horaSaida) {
             System.out.println("A hora de chegada não pode ser maior que a hora de saída!");
             return 1;
-        } else if(horaChegada > 23 || horaSaida > 23 || minutoChegada > 59 || minutoSaida > 59){
+        } else if (horaChegada > 23 || horaSaida > 23 || minutoChegada > 59 || minutoSaida > 59) {
             System.out.println("Hora ou minuto inválidos!!!");
             return 2;
         }
@@ -47,9 +49,9 @@ public class Uni4Exe27{
             horaPagar++;
         }
 
-        if(horaPagar < 3){
+        if (horaPagar < 3) {
             preco = horaPagar * 5;
-        } else if(horaPagar < 5){
+        } else if (horaPagar < 5) {
             preco = (horaPagar - 2) * 7.5f + 10;
         } else {
             preco = (horaPagar - 4) * 10 + 25;
