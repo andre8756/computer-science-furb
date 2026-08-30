9bfdabc9cc61511d5c3f3fe66503620f9e847b7d
andre8756 <schmitt.andre23@gmail.com>
2026-08-30 20:50:12 -0300

    playlist nome e dono prontos

--- Unidade 1/Aula5/sonora-fase02/Playlist.java
+++ Unidade 1/Aula5/sonora-fase02/Playlist.java
@@ -4,42 +4,41 @@ public class Playlist {
     private int quantidadeMax = 100;
     private Musica musicas[] = new Musica[quantidadeMax];
 
-    public Playlist(String nome, Usuario dono){
+    public Playlist(String nome, Usuario dono) {
         setNome(nome);
-        this.dono = dono;
+        setDono(dono);
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    private void setNome(String nome){
+    private void setNome(String nome) {
         if (nome.isBlank() || nome.isEmpty()) {
             throw new IllegalArgumentException("O nome da playlist precisa estar preenchido!");
-        }   
+        }
 
         this.nome = nome;
     }
 
-    public Usuario getDono(){
+    public Usuario getDono() {
         return dono;
     }
 
-    private void setDono(Usuario dono){
+    private void setDono(Usuario dono) {
 
         if (dono == null) {
             throw new IllegalArgumentException("Erro ao vincular dono à playlist.");
         }
 
         this.dono = dono;
-
     }
 
-    public int getQuantidade(){
+    public int getQuantidade() {
         int contador = 0;
 
-        for(int i = 0; i < 99; i++){
-            if(musicas[i] != null){
+        for (int i = 0; i < 99; i++) {
+            if (musicas[i] != null) {
                 contador++;
             }
         }
@@ -47,10 +46,10 @@ public class Playlist {
         return contador;
     }
 
-    public boolean adicionar(Musica musica){
-        
-        for(int i = 0; i < 99; i++){
-            if(musicas[i] == null && musica != null){
+    public boolean adicionar(Musica musica) {
+
+        for (int i = 0; i < 99; i++) {
+            if (musicas[i] == null && musica != null) {
                 musicas[i] = musica;
                 return true;
             }
@@ -59,18 +58,19 @@ public class Playlist {
         return false;
     }
 
-    public Musica getNaPosicao(int indice){
+    public Musica getNaPosicao(int indice) {
         indice--;
 
-        if(indice < 0 || indice > musicas.length){
-            throw new IndexOutOfBoundsException("Erro na getNaPosicao() da Playlist! O índice ("+indice+") é inválido.");
+        if (indice < 0 || indice >= musicas.length) {
+            throw new IndexOutOfBoundsException(
+                    "Erro na getNaPosicao() da Playlist! O índice (" + (indice + 1) + ") é inválido.");
         }
-        
+
         return musicas[indice];
     }
 
-    public boolean removerNaPosicao(int indice){
-        if(indice >= 0 && indice <= 99){
+    public boolean removerNaPosicao(int indice) {
+        if (indice >= 0 && indice <= 99) {
             musicas[indice] = null;
             organizarPlaylist();
             return true;
@@ -78,12 +78,12 @@ public class Playlist {
         return false;
     }
 
-    public int getDuracaoTotalSegundos(){
+    public int getDuracaoTotalSegundos() {
         int duracaoTotalSeg = 0;
 
-        for(int i = 0; i < quantidadeMax; i++){
-            
-            if(musicas[i] != null){
+        for (int i = 0; i < quantidadeMax; i++) {
+
+            if (musicas[i] != null) {
                 duracaoTotalSeg += musicas[i].getDuracaoSegundos();
             }
         }
@@ -91,37 +91,35 @@ public class Playlist {
         return duracaoTotalSeg;
     }
 
-    public void reproduzirTudo(){
+    public void reproduzirTudo() {
 
-        for(int i = 0; i < quantidadeMax; i++){
-            
-            if(musicas[i] != null){
+        for (int i = 0; i < quantidadeMax; i++) {
+
+            if (musicas[i] != null) {
                 musicas[i].reproduzir();
             }
         }
     }
 
-
     // --------- Metodos Auxiliares
 
-    public void organizarPlaylist(){
+    public void organizarPlaylist() {
         boolean troca = true;
         Musica musicaTemp;
 
-        while(troca){
+        while (troca) {
             troca = false;
 
-            for(int i = 0; i < (quantidadeMax - 1); i++){
-                if(musicas[i] == null && musicas[i+1] != null){
+            for (int i = 0; i < (quantidadeMax - 1); i++) {
+                if (musicas[i] == null && musicas[i + 1] != null) {
                     troca = true;
 
                     musicaTemp = musicas[i];
-                    musicas[i] = musicas[i+1];
-                    musicas[i+1] = musicaTemp;
+                    musicas[i] = musicas[i + 1];
+                    musicas[i + 1] = musicaTemp;
                 }
             }
         }
     }
-
 
 }
