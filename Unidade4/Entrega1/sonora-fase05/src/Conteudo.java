33e18b18f2be01dc52e7082683d8d546c657fdb4
andre8756 <schmitt.andre23@gmail.com>
2026-09-23 23:07:55 -0300

    Conteudo

---
+++ Unidade4/Entrega1/sonora-fase05/src/Conteudo.java
@@ -0,0 +1,51 @@
+public class Conteudo {
+    private static int contadorId = 0;
+    private final int id;
+    private String titulo;
+    private int duracaoSegundos;
+
+    public Conteudo(String titulo, int duracaoSegundos) {
+        contadorId++;
+        this.id = contadorId;
+        setTitulo(titulo);
+        setDuracaoSegundos(duracaoSegundos);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        if (titulo == null || titulo.isEmpty()) {
+            throw new IllegalArgumentException("O título não pode estar em branco!");
+        }
+        this.titulo = titulo;
+    }
+
+    public int getDuracaoSegundos() {
+        return duracaoSegundos;
+    }
+
+    public void setDuracaoSegundos(int duracaoSegundos) {
+        if (duracaoSegundos <= 0) {
+            throw new IllegalArgumentException("A música não pode ter menos de 1 segundo!");
+        }
+
+        this.duracaoSegundos = duracaoSegundos;
+    }
+
+    public void reproduzir() {
+        System.out.println("Reproduzindo: " + toString());
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getId() + "] " + titulo
+                + " (" + duracaoSegundos + "s)";
+    }
+
+}
--- Unidade4/Entrega1/sonora-fase05/src/Musica.java
+++ Unidade4/Entrega1/sonora-fase05/src/Musica.java
@@ -1,5 +1,3 @@
-package src;
-
 
 public class Musica {
     private final int id;
